6f17221f269f7a81418f89d98048be439946bb19
dnsalazar10 <dn.salazar10@uniandes.edu.co>
2019-04-02 13:57:09 -0500

    Producto

---
+++ producto.cpp
@@ -0,0 +1,23 @@
+//programa 2: Haga un programa que defina dos variables, una entera y una flotante e imprima el producto de las dos. llamelo producto.cpp
+//a=2
+//b=3.0
+//c=a*b
+//print("el producto de a por b es: ",c)
+
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int i;
+	
+	int a=2;
+	int b=3.0;
+
+
+	int c=a*b;
+	std::cout<<c<<std::endl;
+	return 0;
+}
+
